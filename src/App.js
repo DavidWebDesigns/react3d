00c035634ba58ab5e03a461399bf821da8a6a12c
David Perez <javadavidperez@gmail.com>
2022-04-23 17:54:43 -0500

    react3d experiment

--- src/App.js
+++ src/App.js
@@ -1,25 +1,44 @@
-import logo from './logo.svg';
-import './App.css';
+import ReactDOM from 'react-dom';
+import * as THREE from 'three';
+import { Canvas } from '@react-three/fiber';
+import "./index.css";
+
+  const scene = new THREE.Scene()
+  const camera = new THREE.PerspectiveCamera(75, 100, 0.1, 1000)
+
+function Mainplace () {
+
+
+const renderer = new THREE.WebGLRenderer()
+renderer.setSize(10, 10)
+document.querySelector('#canvas-container').appendChild(renderer.domElement)
+
+const mesh = new THREE.Mesh()
+mesh.geometry = new THREE.BoxGeometry()
+mesh.material = new THREE.MeshStandardMaterial()
+scene.add(mesh)
+
+  function animate() {
+    requestAnimationFrame(animate)
+    renderer.render(scene, camera)
+  }
+return (
+  animate()
+);
+};
+
+
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div id="canvas-container">
+      {/* <Canvas >
+      <Mainplace/>
+        </Canvas> */}
+        
     </div>
-  );
+  )
 }
 
 export default App;
